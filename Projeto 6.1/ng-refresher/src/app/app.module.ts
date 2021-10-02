76f40b72b9d160bb675320c2600075cd42656fbc
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-02 14:27:12 -0300

    Angular refresh

---
+++ Projeto 6.1/ng-refresher/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonsComponent,
+    PersonInputComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
