3f0b95cb033bb8da4e8a37ddb6423d6d12a512a2
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-04 13:07:09 -0300

    My first ionic app

--- Projeto 6.1/ng-refresher/src/app/persons/person.service.ts
+++ Projeto 6.1/ng-refresher/src/app/persons/person.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@angular/core";
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PersonService {
+    pessoasAlteração = new Subject<string[]>();
+
     persons: string[] = ['Max', 'Manuel', 'Anna'];
 
     addPessoa(name: string) {
         this.persons.push(name)
-        console.log(this.persons)
+        this.pessoasAlteração.next(this.persons);
+    }
+
+    removePerson(name: string) {
+        this.persons = this.persons.filter(person => {
+            return person !== name;
+        });
+
+        this.pessoasAlteração.next(this.persons);
     }
 }
--- Projeto 6.1/ng-refresher/src/app/persons/persons.component.ts
+++ Projeto 6.1/ng-refresher/src/app/persons/persons.component.ts
@@ -1,16 +1,30 @@
 import { PersonService } from './person.service';
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-persons',
     templateUrl: './persons.component.html'
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
     personList!: string[];
+
+    private personListSubs!: Subscription;
 
     constructor(private prsService: PersonService) {}
 
     ngOnInit() {
         this.personList = this.prsService.persons;
+        this.personListSubs = this.prsService.pessoasAlteração.subscribe((persons => {
+            this.personList = persons;
+        }));
+    }
+
+    onRemovePerson(personName: string) {
+        this.prsService.removePerson(personName);
+    }
+
+    ngOnDestroy() {
+        this.personListSubs.unsubscribe();
     }
 }
