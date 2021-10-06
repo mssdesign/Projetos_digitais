e7bc7e69c2e41b6c20a525528a1149fdab925e77
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-05 22:01:58 -0300

    My first ionic app

--- Projeto 6.1/ng-refresher/src/app/persons/person.service.ts
+++ Projeto 6.1/ng-refresher/src/app/persons/person.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
@@ -11,7 +12,9 @@ export class PersonService {
     constructor(private http: HttpClient) {};
 
     fetchPersons() {
-        this.http.get<any>('https://swapi.dev/api/people').subscribe(resData => console.log(resData));
+        this.http.get<any>('https://swapi.dev/api/people').pipe(map(resData => {
+            return resData.results
+        })).subscribe(DadoFinal => console.log(DadoFinal));
     }
 
     addPessoa(name: string) {
