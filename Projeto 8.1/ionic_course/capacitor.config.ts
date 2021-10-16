41bb82448db72be9d590362282b84b111cd223ef
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-16 07:08:35 -0300

    App de receitas

--- Projeto 8.1/ionic_course/capacitor.config.ts
+++ Projeto 8.1/ionic_course/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.receitas_first.app',
   appName: 'ionic_course',
   webDir: 'www',
   bundledWebRuntime: false
