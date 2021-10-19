d3e8de5a4cc09b041b90719f981a2da8fafb5ba0
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-19 10:03:52 -0300

    airbnb2

--- Projeto_8.2/airbnb2/src/app/places/discover/place-detail/place-detail.page.ts
+++ Projeto_8.2/airbnb2/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
-import { Router } from '@angular/router';
+import { Place } from './../../offers/place.model';
+import { PlacesService } from './../../places.service';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
@@ -8,10 +10,20 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private route: ActivatedRoute, private placesService: PlacesService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get('placeId'))
+    });
+  }
 
   onBookPlace() {
     //this.router.navigateByUrl('/places/tabs/discover'); //Animação errada
--- Projeto_8.2/airbnb2/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ Projeto_8.2/airbnb2/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,3 +1,4 @@
+import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { Place } from './../place.model';
 import { PlacesService } from './../../places.service';
@@ -11,10 +12,15 @@ import { Component, OnInit } from '@angular/core';
 export class EditOfferPage implements OnInit {
   placeEdit: Place;
 
-  constructor(private placesService: PlacesService, private route: ActivatedRoute) { }
+  constructor(private placesService: PlacesService, private route: ActivatedRoute, private navCtrl: NavController) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+
       this.placeEdit = this.placesService.getPlace(paramMap.get('placeId'))
     });
   }
