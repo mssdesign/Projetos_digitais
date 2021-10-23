f654ca6f283be20e40c6e9dd0d73792916b59eb8
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-23 10:38:37 -0300

    airbnb2

--- Projeto_8.2/airbnb2/src/app/app.component.ts
+++ Projeto_8.2/airbnb2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {}
+  constructor(private menuCtr: MenuController) {}
 
   onLogout() {}
+
+  closeMenu() {
+    this.menuCtr.close('m1')
+  }
   
 }
