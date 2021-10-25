822480c7f47cbe2eda03475f42e5c8a25a544433
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-10-25 12:39:30 -0300

    first angular project

---
+++ angular_revisao_app_receita/ionic_course/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.receitas_first.app',
+  appName: 'ionic_course',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
